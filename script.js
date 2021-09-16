bee8111ec52d9aa0b88e71b439dba060d8fc75f3
Dharshan.K <60266338+dharshank100@users.noreply.github.com>
2021-09-16 11:26:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+});
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+    });
+});
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Web Designer", "Freelancer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Web Developer", "Web Designer", "App Developer", "Freelancer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
